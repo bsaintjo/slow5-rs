0bb83fca56e12ac9f813b62aa5f4cbe415c5127d
Brandon Saint-John <bsaintjo@ucsc.edu>
2022-11-10 12:45:16 -0800

    Added example idx file, update tests

--- tests/read_blow5.rs
+++ tests/read_blow5.rs
@@ -3,7 +3,7 @@ use slow5::{FileReader, RecordExt};
 #[test]
 fn main() -> anyhow::Result<()> {
     let file_path = "examples/example3.blow5";
-    let slow5 = FileReader::open(file_path)?;
+    let mut slow5 = FileReader::open(file_path)?;
     let mut records = slow5.records();
     while let Some(Ok(rec)) = records.next() {
         println!("{:?}", rec.read_id());
