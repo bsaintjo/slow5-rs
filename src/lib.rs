cf957c3772beb95fdc1930136716e50360dc58de
Brandon Saint-John <bsaintjo@ucsc.edu>
2022-12-01 10:40:35 -0800

    expand with larger test

--- src/lib.rs
+++ src/lib.rs
@@ -29,3 +29,28 @@ pub(crate) fn to_cstring<T: Into<Vec<u8>>>(x: T) -> Result<CString, Slow5Error> 
 pub(crate) fn to_cstr<T: AsRef<[u8]>>(x: &T) -> Result<&CStr, Slow5Error> {
     CStr::from_bytes_with_nul(x.as_ref()).map_err(Slow5Error::NulError)
 }
+
+#[cfg(test)]
+mod test {
+    use assert_fs::{prelude::PathChild, TempDir};
+
+    use super::*;
+
+    #[test]
+    fn test_rw() -> anyhow::Result<()> {
+        let tmp_dir = TempDir::new()?;
+        let filepath = tmp_dir.child("new.slow5");
+
+        let mut opts = WriteOptions::default();
+        opts.aux("median", FieldType::Float)
+            .aux("read_number", FieldType::Uint32)
+            .attr("attr", "val", 0)
+            .attr("attr", "other", 1);
+        let writer = FileWriter::with_options(filepath, opts)?;
+        let header = writer.header();
+        assert_eq!(header.get_attribute("attr", 0)?, b"val");
+        assert_eq!(header.get_attribute("attr", 1)?, b"other");
+        assert_eq!(header.aux_names_iter()?.count(), 2);
+        Ok(())
+    }
+}
