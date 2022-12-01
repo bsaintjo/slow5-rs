bb72acf1130bdd81eac7e6adf12fa0265759ab41
Brandon Saint-John <bsaintjo@ucsc.edu>
2022-12-01 00:05:16 -0800

    formating in example

--- examples/auxiliary_field.rs
+++ examples/auxiliary_field.rs
@@ -5,7 +5,7 @@ fn main() -> Result<()> {
     let mut slow5 = FileReader::open("examples/example2.slow5")?;
     let rec = slow5.records().next().unwrap()?;
     let median_before: f64 = rec.get_aux_field("median_before")?;
-    println!("median_before = {median_before}");
+    println!("median_before = {median_before:.2}");
 
     let start_time: u64 = rec.get_aux_field("start_time")?;
     println!("start_time = {start_time}");
