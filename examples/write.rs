8bc6d229259f4e82f376ae456f0dfabaaefce2c4
Brandon Saint-John <bsaintjo@ucsc.edu>
2022-11-30 23:58:03 -0800

    fix write example

--- examples/write.rs
+++ examples/write.rs
@@ -7,15 +7,16 @@ fn main() -> Result<()> {
     let file_path = tmp_dir.join("test.blow5");
     let mut opts = WriteOptions::default();
     opts.attr("run_id", "run_0", 0)
-        .attr("asic_id", "asic_id_0", 0);
+        .attr("asic_id", "asic_id_0", 0)
+        .aux("median", FieldType::Float);
     let mut slow5 = FileWriter::with_options(file_path, opts)?;
 
-    let mut hdr = slow5.header();
-    set_header_aux_fields(&mut hdr)?;
+    let hdr = slow5.header();
     let rec = set_record_fields(&hdr)?;
     slow5.add_record(&rec)?;
     slow5.close();
-
+    
+    println!("Success!");
     Ok(())
 }
 
@@ -30,9 +31,4 @@ fn set_record_fields(hdr: &Header) -> Result<Record> {
         .build()?;
     rec.set_aux_field(hdr, "median", 1.2f32)?;
     Ok(rec)
-}
-
-fn set_header_aux_fields(hdr: &mut Header) -> Result<()> {
-    hdr.add_aux_field("median", FieldType::Float)?;
-    Ok(())
 }
