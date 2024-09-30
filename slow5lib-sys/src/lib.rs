6842c55c1a81811e3c69a19900f59e44723c9b55
Brandon T. P. Saint-John <saint-john@lbl.gov>
2024-09-30 16:57:02 -0700

    allow unportable markdown from slow5lib docs

--- slow5lib-sys/src/lib.rs
+++ slow5lib-sys/src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
+#![allow(rustdoc::unportable_markdown)]
 #![doc = include_str!("../README.md")]
 
 use libc::*;
