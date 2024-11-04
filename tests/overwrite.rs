d456bc3348b0e314f14af9c8d87061c351f96139
Brandon T. P. Saint-John <saint-john@lbl.gov>
2024-11-04 14:18:57 -0800

    Finish up overwrite test

--- tests/overwrite.rs
+++ tests/overwrite.rs
@@ -1,10 +1,17 @@
 use std::path::Path;
 
-use assert_fs::TempDir;
-use slow5::{FieldType, FileWriter, RecordBuilder, RecordCompression, SignalCompression};
+use anyhow::Ok;
+use assert_fs::{prelude::PathChild, TempDir};
+use slow5::{
+    FieldType, FileReader, FileWriter, RecordBuilder, RecordCompression, RecordExt,
+    SignalCompression,
+};
 
-fn write_test_file(file_path: &Path, signal_comp: SignalCompression, rec_comp: RecordCompression) {
-    // let file_path = tmp_dir.child(format!("new_{rec_idx}_{sig_idx}.blow5"));
+fn write_test_file(
+    file_path: &Path,
+    signal_comp: SignalCompression,
+    rec_comp: RecordCompression,
+) -> anyhow::Result<()> {
     let mut writer = FileWriter::options()
         .attr("attr", "val", 0)
         .attr("attr", "other", 1)
@@ -16,8 +23,7 @@ fn write_test_file(file_path: &Path, signal_comp: SignalCompression, rec_comp: R
         .aux("not set", FieldType::Uint16)
         .signal_compression(signal_comp)
         .record_compression(rec_comp)
-        .create(file_path)
-        .unwrap();
+        .create(file_path)?;
 
     let mut builder = RecordBuilder::default();
     builder
@@ -32,17 +38,46 @@ fn write_test_file(file_path: &Path, signal_comp: SignalCompression, rec_comp: R
             .read_id(id)
             .read_group(i)
             .raw_signal(&signals[i as usize])
-            .build()
-            .unwrap();
-        rec.set_aux_field(&mut writer, "median", 10.0f32).unwrap();
-        rec.set_aux_field(&mut writer, "read_number", 7u32).unwrap();
-        rec.set_aux_field(&mut writer, "string", "here").unwrap();
+            .build()?;
+        rec.set_aux_field(&mut writer, "median", 10.0f32)?;
+        rec.set_aux_field(&mut writer, "read_number", 7u32)?;
+        rec.set_aux_field(&mut writer, "string", "here")?;
         writer.add_record(&rec).unwrap();
     }
     writer.close();
+    Ok(())
 }
 
 #[test]
 fn test_overwrite() {
     let tmp_dir = TempDir::new().unwrap();
+    if let Err(e) = (|| {
+        let example = tmp_dir.child("example.blow5");
+        write_test_file(&example, SignalCompression::None, RecordCompression::None)?;
+        write_test_file(
+            &example,
+            SignalCompression::StreamVByte,
+            RecordCompression::ZStd,
+        )?;
+
+        let reader = FileReader::open(&example)?;
+        anyhow::ensure!(reader.signal_compression() == SignalCompression::StreamVByte);
+        anyhow::ensure!(reader.record_compression() == RecordCompression::ZStd);
+        let rec = reader.get_record("read_2")?;
+        anyhow::ensure!(rec.read_group() == 2);
+
+        let signals = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+        assert_eq!(
+            rec.raw_signal_iter().collect::<Vec<_>>(),
+            signals[2].to_vec()
+        );
+        anyhow::ensure!(rec.get_aux_field::<f32>("median").unwrap() == 10.0f32);
+        anyhow::ensure!(rec.get_aux_field::<&str>("string").unwrap() == "here");
+        anyhow::ensure!(rec.get_aux_field::<i16>("not set").is_err());
+        anyhow::Result::Ok(())
+    })() {
+        eprintln!("Error: {e:?}");
+    } else {
+        println!("Success!");
+    }
 }
